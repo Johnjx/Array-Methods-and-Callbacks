53f8d54fec68c4f66c53385b1c5a6bfa42885f3f
John Ruddock <john.jxr@pm.me>
2022-03-22 15:31:27 -0400

    Completed Task 3: created higher order function returning years array be passing in the original data set and the get finals function

--- index.js
+++ index.js
@@ -47,10 +47,12 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function as the second parameter that will take getFinals from task 2 as an argument
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, callBack) {
+    const years = callBack(array).map(data => data.Year);
+    return years;
 }
 
+console.log('Task 3:', getYears(fifaData, getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
